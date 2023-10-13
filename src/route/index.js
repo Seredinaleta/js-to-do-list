a1a9e20f41930837133488fc6041dfd48ee50099
Bill Gates <yuliia.trubina.qa@gmail.com>
2023-10-13 10:20:28 +0300

    Task is done

--- src/route/index.js
+++ src/route/index.js
@@ -4,10 +4,12 @@ const router = express.Router()
 
 // Підключіть файли роутів
 const test = require('./test')
+const todo = require('./todo')
 // Підключіть інші файли роутів, якщо є
 
 // Об'єднайте файли роутів за потреби
 router.use('/', test)
+router.use('/', todo)
 // Використовуйте інші файли роутів, якщо є
 
 // Експортуємо глобальний роутер
